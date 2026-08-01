89d06b7a3f6fe2a373c564863069f54105351098
Rupam Ghosh <rupandos@users.noreply.github.com>
2026-08-02 00:21:29 +0530

    chore: add ESLint and Prettier configuration with strict TypeScript rules

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  env: {
+    node: true,
+    es2022: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  ignorePatterns: ['dist/', 'node_modules/', 'coverage/', 'prisma/migrations/'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': 'warn',
+    'no-constant-condition': ['error', { checkLoops: false }],
+  },
+};
